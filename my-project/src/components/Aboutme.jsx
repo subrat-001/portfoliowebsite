64cd7144dcd6476bc1ff1eeb005d196a5bb5b986
Subrat Sharma <87660062+subrat-001@users.noreply.github.com>
2024-02-26 17:52:42 +0545

    this

--- my-project/src/components/Aboutme.jsx
+++ my-project/src/components/Aboutme.jsx
@@ -9,15 +9,14 @@ const Aboutme = () => {
             <div className="w-100 py-2 text-center">
                 <h3 className=" text-8xl font-bold px-4 my-auto mx-auto font-serif py-2">About Me</h3>
                 <img src="https://media.licdn.com/dms/image/D5603AQGtriF0shY6sQ/profile-displayphoto-shrink_800_800/0/1703308911847?e=1714608000&v=beta&t=0OwKqA54VwgmgJ8_ODl7F5iT5oWmNoJnmHYIVEYe19o" className=" max-h-72 rounded-full mx-auto py-2"/>
-                <h3 className=" text-4xl font-bold my-auto mx-auto font-serif py-2">Subrat Sharma</h3>
+                <h3 className=" text-3xl font-bold my-auto mx-auto font-serif py-2">Subrat Sharma</h3>
                 <a href="https://drive.google.com/file/d/15rGnvHfZ-QcXiZhrlIxrJCkzkPBGJWBC/view?usp=drive_link" target="_blank"> <h1 className="text-xl py-2">Resume</h1></a>
             </div>
-            <div className="w-100 text-center"> 
-            <h3 className=" text-3xl font-bold my-auto py-4 mx-auto font-serif">Education</h3>
-            </div>
+  
             <div className="w-100 text-center">
-              
-            <div className="w-100 flex flex-row py-auto my-auto justify-evenly">
+                <h3 className=" text-3xl font-bold my-auto py-4 mx-auto font-serif">Education</h3>
+
+            <div className="w-100 flex flex-row py-4 my-auto justify-evenly">
             <h1 className="text-xl">Computer Engineering<br/>Thapathali Campus</h1>
                     <h1 className="text-xl">High School Science<br />Khwopa Collage</h1>
                     <h1 className="text-xl">Base Education<br />Vidhya Sagar School</h1>
--- my-project/src/components/Projects.jsx
+++ my-project/src/components/Projects.jsx
@@ -1,78 +1,80 @@
-
 import { useEffect, useState } from "react";
+
 const Projects = () => {
-
-
     return (
-        <div id="projects" className=" flex text-black flex-col justify-evenly max-w-screen min-w-screen min-h-screen max-h-screen bg-orange-400">
-
-            <div className="w-100 text-center" >
-                <h1 className=" text-8xl font-bold px-4 my-auto mx-auto font-serif">Projects</h1>
+        <div id="projects" className="flex text-black flex-col justify-evenly max-w-screen min-w-screen min-h-screen max-h-screen bg-orange-400">
+            <div className="w-100 text-center">
+                <h1 className="text-8xl font-bold px-4 my-auto mx-auto font-serif">Projects</h1>
             </div>
-            <div className=" w-100">
+            <div className="w-100">
                 <div className="flex flex-row justify-around w-100">
-
-
-                    <div className=" my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
-                        <div className=" row-span-3">
-                            <img className=" w-full h-auto max-h-48" src="https://cdn.pixabay.com/photo/2013/07/13/10/51/football-157930_1280.png" alt='football' />
+                    <div className="my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
+                        <div className="row-span-3">
+                            <a href="https://github.com/subrat-001/recomendation_developing" target="_blank">
+                                <img className="w-auto mx-auto my-auto h-auto max-h-48" src="https://cdn.pixabay.com/photo/2013/07/13/10/51/football-157930_1280.png" alt='football' />
+                            </a>
                         </div>
-                        <div className=" row-span-1">
-                            <a href="https://github.com/subrat-001/recomendation_developing"><h3 className=" text-center  font-semibold text-l">Player Recomendation System</h3></a>
-                        </div>
-                    </div>
-
-                    <div className=" my-4 mx-4  grid grid-rows-4 justify-center w-1/4">
-                        <div className=" row-span-3">
-                            <img className="w-full h-auto max-h-56" src="https://cdn.pixabay.com/photo/2014/03/24/17/15/cricket-ball-295206_1280.png" alt="Cricket Data Analysis" />
-                        </div>
-                        <div className=" row-span-1">
-                            <a href="https://github.com/subrat-001/wbbl_analysis"><h3 className=" text-center  font-semibold text-l">Cricket Data Analysis</h3></a>
+                        <div className="row-span-1">
+                            <h3 className="text-center font-semibold text-xl">Player Recommendation System</h3>
                         </div>
                     </div>
 
-                    <div className=" my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
-                        <div className=" row-span-3">
-                            <img className="w-full h-auto max-h-56" src="https://cdn.pixabay.com/photo/2017/01/31/19/57/cartoon-2026834_1280.png" alt="Blockchain Based Supplychain Mangaement" />
+
+                    <div className="my-4 mx-4 grid grid-row-4 justify-center w-1/4">
+                        <div className="row-span-3">
+                            <a href="https://github.com/subrat-001/boats" target="_blank">
+                                <img className="w-auto mx-auto my-auto h-auto max-h-48 min-h-48" src="https://cdn.pixabay.com/photo/2020/04/11/06/08/ship-5028940_1280.png" alt="Dunga" />
+                            </a>
                         </div>
-                        <div className=" row-span-1">
-                            <a href="https://github.com/subrat-001/projectmain"><h3 className=" text-center  font-semibold text-l">Blockchain Based Supplychain Mangaement</h3></a>
+                        <div className="row-span-1">
+                            <h3 className="text-center font-semibold text-xl">Dunga-Boat Game</h3>
+                        </div>
+                    </div>
+
+                    <div className="my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
+                        <div className="row-span-3">
+                            <a href="https://github.com/subrat-001/wbbl_analysis" target="_blank">
+                                <img className="w-auto mx-auto my-auto h-auto max-h-48 min-h-48" src="https://cdn.pixabay.com/photo/2014/03/24/17/15/cricket-ball-295206_1280.png" alt="Cricket Data Analysis" />
+                            </a>
+                        </div>
+                        <div className="row-span-1">
+                            <h3 className="text-center font-semibold text-xl">Cricket Data Analysis</h3>
                         </div>
                     </div>
 
 
                 </div>
             </div>
+
             <div>
-                <div className=" w-100">
+                <div className="w-100">
                     <div className="flex flex-row justify-around w-100">
-
-
-                        <div className=" my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
-                            <div className=" row-span-3">
-                                <img className="w-full h-auto max-h-56" src="https://cdn.pixabay.com/photo/2016/10/27/09/35/book-1773756_1280.png" alt="LMS" />
+                        <div className="my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
+                            <div className="row-span-3">
+                                <a href="https://github.com/subrat-001/LMS" target="_blank">
+                                    <img className="w-auto mx-auto my-auto h-auto max-h-48 min-h-48" src="https://cdn.pixabay.com/photo/2012/04/13/15/04/library-32746_1280.png" alt="LMS" />
+                                </a>
                             </div>
-                            <div className=" row-span-1">
-                                <a href="https://github.com/subrat-001/LMS"><h3 className=" text-center  font-semibold text-l">Library Management System</h3></a>
+                            <div className="row-span-1">
+                                <h3 className="text-center font-semibold text-xl">Library Management System</h3>
                             </div>
                         </div>
 
-                        <div className=" my-4 mx-4 grid grid-row-4 justify-center w-1/4">
-                            <div className=" row-span-3">
-                                <img className="w-full h-auto max-h-56" src="https://cdn.pixabay.com/photo/2017/10/09/21/40/boat-2835005_1280.png" alt="Dunga" />
+                        <div className="my-4 mx-4 grid grid-rows-4 justify-center w-1/4">
+                            <div className="row-span-3">
+                                <a href="https://github.com/subrat-001/projectmain" target="_blank">
+                                    <img className="w-auto mx-auto my-auto h-auto max-h-48 min-h-48" src="https://cdn.pixabay.com/photo/2017/01/31/19/57/cartoon-2026834_1280.png" alt="Blockchain Based Supplychain Management" />
+                                </a>
                             </div>
-                            <div className=" row-span-1">
-                                <a href="https://github.com/subrat-001/boats"><h3 className=" text-center  font-semibold text-l">Dunga-Boat Game</h3></a>
+                            <div className="row-span-1">
+                                <h3 className="text-center font-semibold text-xl">Blockchain Based Supplychain Management</h3>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
-    )
+    );
 };
-
 
 export default Projects;
